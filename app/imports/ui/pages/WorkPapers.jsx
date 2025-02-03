dce8bd019913ca48c4297ad34c4c4f056357e4a4
Robert Maddox <39988090+robertmaddoxhi@users.noreply.github.com>
2025-02-02 18:45:11 -1000

    Fix table display

--- app/imports/ui/pages/WorkPapers.jsx
+++ app/imports/ui/pages/WorkPapers.jsx
@@ -1,21 +1,38 @@
 import React from 'react';
-import { Button, Container, Table } from 'react-bootstrap';
+import { Container, Table } from 'react-bootstrap';
 import { PAGE_IDS } from '../utilities/PageIDs';
 
-const WorkPapers = () => {
-
-  <div className="table-responsive">
-    <Button variant="outline-primary" className="m-3">Stress Test 1</Button>
-    <Table className="table table-striped table-hover">
-      <thead>
-        <th scope="col">Year</th>
-        <th scope="col">Balance</th>
-        <th scope="col">Yearly Contribution</th>
-        <th scope="col">Interest Earned</th>
-        <th scope="col">Interest + Balance</th>
-      </thead>
-    </Table>
-  </div>;
-};
-
+const WorkPapers = () => (
+  <Container id={PAGE_IDS.WORKPAPERS} className="py-3">
+    <div className="table-responsive">
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th scope="col">Year</th>
+            <th scope="col">Balance</th>
+            <th scope="col">Yearly Contribution</th>
+            <th scope="col">Interest Earned</th>
+            <th scope="col">Interest + Balance</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <th scope="row">50,000</th>
+            <th scope="row">50,000</th>
+            <th scope="row">2,100</th>
+            <th scope="row">52,100</th>
+          </tr>
+          <tr>
+            <th scope="row">1</th>
+            <th scope="row">50,000</th>
+            <th scope="row">50,000</th>
+            <th scope="row">2,100</th>
+            <th scope="row">52,100</th>
+          </tr>
+        </tbody>
+      </Table>
+    </div>
+  </Container>
+);
 export default WorkPapers;
