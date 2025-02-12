0eb51a2692aeee10c22080ce900e966c99c86d47
Adam Bell <belladam@hawaii.edu>
2025-02-12 01:51:06 -1000

    Update acceptance tests for new landing

--- app/tests/landing.page.js
+++ app/tests/landing.page.js
@@ -6,27 +6,16 @@ class LandingPage {
     this.pageId = `#${PAGE_IDS.LANDING}`;
     this.pageSelector = Selector(this.pageId);
     this.carousel = Selector('.landing-carousel');
-    this.carouselItems = this.carousel.find('.carousel-item');
     this.footerLink = Selector('.footer #project-webpage');
   }
 
   /* Asserts that this page is currently displayed. */
   async isDisplayed() {
-    // From https://testcafe.io/documentation/402803/recipes/best-practices/create-helpers
-    // Note that this file imports t (the test controller) from the testcafe module. You don’t need to pass t to helper functions because TestCafe can resolve the current test context and provide the correct test controller instance.
     await t.expect(this.pageSelector.exists).ok();
-  }
-
-  async checkCarousel() {
-    await t.expect(this.carousel.visible).ok('Landing carousel is visible.');
-
-    const itemsCount = await this.carouselItems.count;
-    await t.expect(itemsCount).gt(0, 'Carousel has items.');
   }
 
   async checkFooterLink() {
     const url = await this.footerLink.getAttribute('href');
-
     await t.expect(url === 'https://kala-forecast.github.io/').ok('GitHub Page link is correct.');
   }
 }
